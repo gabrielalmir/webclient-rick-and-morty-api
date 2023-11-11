417ff40d666dc2b6f63804415ed72909fe9f072b
gabrielalmir <gabr.almir@gmail.com>
2023-11-11 11:35:06 -0300

    chore: transform location dto in record

--- src/main/java/br/com/gabrielalmir/webclientrickandmortyapi/location/dtos/LocationDto.java
+++ src/main/java/br/com/gabrielalmir/webclientrickandmortyapi/location/dtos/LocationDto.java
@@ -4,14 +4,12 @@ import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 
-import lombok.Getter;
-
-@Getter
 @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
-public class LocationDto {
-    private String id;
-    private String name;
-    private String type;
-    private List<String> residents;
-    private String url;
+public record LocationDto(
+    String id,
+    String name,
+    String type,
+    List<String> residents,
+    String url
+) {
 }
