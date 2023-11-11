068ffd8dcc6b2a5c5f51b1b9cba70a44f7c602d1
gabrielalmir <gabr.almir@gmail.com>
2023-11-11 11:12:57 -0300

    chore: remove setter in characters

--- src/main/java/br/com/gabrielalmir/webclientrickandmortyapi/character/response/CharacterResponse.java
+++ src/main/java/br/com/gabrielalmir/webclientrickandmortyapi/character/response/CharacterResponse.java
@@ -5,9 +5,7 @@ import java.util.List;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 
 import lombok.Getter;
-import lombok.Setter;
 
-@Setter
 @Getter
 @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
 public class CharacterResponse {
